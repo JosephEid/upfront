bd5d42c53ec11dfe4220ebca273b6010179a1a84
JosephEid <josephceid@gmail.com>
2021-07-10 19:22:14 +0100

    form

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Button from 'react-bootstrap/Button';
-
+import { Form, Col, Row } from 'react-bootstrap';
 import './Home.css';
 
 const Home = () => {
+  const salaryValues = Array.from({length: 10}, (_, i) => i + 1);
+
+  const dropdownOptions = salaryValues.map((value) => 
+    <option value={value*10000}>{`£${value}0,000`}</option>
+  );
+
   return (
     <Jumbotron>
       <h1>Hello!</h1>
@@ -12,7 +17,28 @@ const Home = () => {
         Welcome to HonestJobs, where all Job Postings must contain a salary range.
       </p>
       <p>
-        <Button variant="primary">Learn more</Button>
+      <Form class="job-search-form">
+        <Row>
+          <Col>
+            <Form.Control placeholder="Job title, keywords or company" />
+          </Col>
+          <Col>
+            <Form.Control placeholder="Where" />
+          </Col>
+          <Col>
+            <Form.Control as="select">
+              <option>No min</option>
+              {dropdownOptions}
+            </Form.Control>
+          </Col>
+          <Col>
+            <Form.Control as="select">
+              <option>No max</option>
+              {dropdownOptions}
+            </Form.Control>
+          </Col>
+        </Row>
+      </Form>
       </p>
     </Jumbotron>
   );

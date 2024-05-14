fddffd9137e452d9c8a7f9ac28eafaf90f55cae3
Joseph Eid <josephceid@gmail.com>
2024-05-14 17:12:06 +0100

    navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    const theme = extendTheme({
+        colors: {
+            upfront: {
+                200: "#9ad9d4",
+                300: "#8cbdb8",
+            },
+        },
+    });
+    return (
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 }
